9e51dc3d358c5f560a611c2099a87f056dfdc1e0
Anna WASBAUER <anna.wasbauer@ens.uvsq.fr>
2024-01-02 21:17:40 +0100

    correction requêtes

--- Requetes.sql
+++ Requetes.sql
@@ -1,3 +1,5 @@
+SET SERVEROUTPUT ON;
+
 -- 1
 SELECT c.TitreContenu
 FROM Contenu c, Professionnel pr, Participe pa
@@ -18,6 +20,14 @@ AND pa1.IDpro = p.IDPro
 
 HAVING COUNT(DISTINCT ) = 1;
 
+-- 4
+SELECT pr2.NomPro, pr2.PrenomPro
+FROM Participe pa1, Participe pa2, Professionnel pr1, Professionnel pr2
+WHERE pr1.NomPro = 'Depp' AND pr1.PrenomPro = 'Johnny'
+AND pa2.Profession = 'Réalisateur' AND pa1.IDContenu = pa2.IDContenu
+AND pa1.IDPro = pr1.IDPro  AND pa2.IDPro = pr2.IDPro;
+/
+
 -- 5
 SELECT COUNT(*)
 FROM Client
@@ -28,7 +38,7 @@ WHERE TypeAbonnement = 'Etudiant';
 /* moyenne d'age des gens qui ont regardé tous les films ou séries  keira knightley a joué ou a réalisé
 personne (table profil) telle qu'il n'existe pas de contenu dans lequel keira est (tables contenu/participe) tel que la personne a pas vu (tables lecturefilm, lectureep) */
 
-SELECT AVG(AgeProfil)
+SELECT AVG(months_between(SYSDATE, AgeProfil)/12) as Age_Moyen
 FROM Profil p
 WHERE NOT EXISTS (select pa.IDContenu
 		from Participe pa, Professionnel pr
@@ -47,32 +57,58 @@ WHERE NOT EXISTS (select pa.IDContenu
 -- - on met tous ces épisodes dans une table avec le titre de la série, le numsaison, le numepisode, le titre de l'épisode et la date de sortie de l'épisode
 
 -- 12
-SELECT a.IDClient, a.IDProfil, cl.TypeAbonnement, COUNT(cl.TypeAbonnement)
+SELECT cl.TypeAbonnement, COUNT(*)
 FROM Avis a, Participe pa, Professionnel pr, Client cl
-WHERE pr.Nom = 'Dwayne The Rock' AND pr.Prenom = 'Johnson'
+WHERE pr.NomPro = 'Dwayne The Rock' AND pr.PrenomPro = 'Johnson'
 AND pa.Profession = 'Acteur' AND pa.Role = 'Principal'
-AND a.IDContenu = pr.IDContenu AND pa.IDPro = pr.IDPro
+AND a.IDContenu = pa.IDContenu AND pa.IDPro = pr.IDPro
 AND a.IDClient = cl.IDClient
-GROUP BY cl.TypeAbonnement;
+GROUP BY TypeAbonnement;
 /
 
 -- 13
-SELECT temp1.Famille, temp2.Standard
-FROM (select p.IDClient, p.IDProfil (count(p.IDProfil)/count(distinct p.IDClient)) into Famille
-	from Client c, Profil p
-	where c.TypeAbonnement = 'Famille' and p.TypeProfil = 'Enfant'
-	and c.IDClient = p.IDClient) temp 1,
-(select p.IDClient, p.IDProfil (count(p.IDProfil)/count(distinct p.IDClient)) into Standard
-	from Client c, Profil p
-	where c.TypeAbonnement = 'Standard' and p.TypeProfil = 'Enfant'
-	and c.IDClient = p.IDClient) temp2;
+DECLARE
+    n	int := 1;
+	m	int := 1;
+	f	int;
+	s	int;
+	a	int;
+	b	int;
+BEGIN
+    SELECT COUNT(p.IDProfil) into f
+    FROM Client c, Profil p
+    WHERE c.TypeAbonnement = 'Famille' AND p.TypeProfil = 'Enfant'
+    AND c.IDClient = p.IDClient;
+	SELECT COUNT(DISTINCT p.IDClient) into a
+    FROM Client c, Profil p
+    WHERE c.TypeAbonnement = 'Famille' AND p.TypeProfil = 'Enfant'
+    AND c.IDClient = p.IDClient;
+    IF a != 0 THEN n := a;
+    END IF;
+	f := f/n;
+
+	SELECT COUNT(p.IDProfil) into s
+	FROM Client c, Profil p
+    WHERE c.TypeAbonnement = 'Standard' AND p.TypeProfil = 'Enfant'
+    AND c.IDClient = p.IDClient;
+	SELECT COUNT(DISTINCT p.IDClient) into b
+	FROM Client c, Profil p
+    WHERE c.TypeAbonnement = 'Standard' AND p.TypeProfil = 'Enfant'
+    AND c.IDClient = p.IDClient;
+    IF b != 0 THEN n := b;
+    END IF;
+	s := s/n;
+
+	dbms_output.put_line('Le nombre moyen de profil Enfant par compte Famille est ', || f);
+	dbms_output.put_line('Le nombre moyen de profil Enfant par compte Standard est ', || s);
+END;
 / 
 
 -- 14
 /* la conception permet pas de savoir si on épisode a été regardé 2 fois, sauf si c'est 2 jours différents donc on va group by et rassembler les mêmes épisodes */
 
 CREATE OR REPLACE FUNCTION nbepisode_profil (id_cli IN Profil.IDProfil%type, id_prof IN Profil.IDProfil%type) RETURN NUMBER IS
-n	NUMBER
+n	NUMBER;
 BEGIN
 	SELECT COUNT(*) INTO n
 	FROM LectureEP
@@ -83,7 +119,7 @@ END;
 /
 
 -- 15
-SELECT AVG(pr.Age)
+SELECT AVG(months_between(SYSDATE, pr.AgePro)/12) as Age_Moyen
 FROM Participe pa, Professionnel pr, Films f
 WHERE f.TitreContenu = 'Avengers' AND pa.Profession = 'Acteur'
 AND pa.IDPro = pr.IDPro AND pa.IDContenu = f.IDContenu;
@@ -97,7 +133,7 @@ AND pa.IDContenu = f.IDContenu;
 /
 
 -- 18
-personne (table profil) telle qu'il n'existe pas de film (vue film) tel que la personne a pas vu (table lecturefilm)
+-- personne (table profil) telle qu'il n'existe pas de film (vue film) tel que la personne a pas vu (table lecturefilm)
 
 SELECT IDClient, IDProfil, Pseudo
 FROM Profil
@@ -123,12 +159,11 @@ HAVING COUNT(a.IDContenu) >= 30;
 /
 
 -- 23
-SELECT f.TitreContenu, COUNT(DISTINCT l.IDContenu)
-FROM LectureFilm l, Films f
-WHERE l.DateRegarde LIKE '%-jul-2023'
-AND l.IDContenu = f.IDContenu
-GROUP BY l.IDContenu;
-/
+SELECT l.IDContenu, COUNT(DISTINCT l.IDContenu)
+FROM LectureFilm l FULL JOIN Films f ON l.IDContenu = f.IDContenu
+WHERE to_char(l.DateLecture) LIKE '%-jul-2023' AND ROWNUM = 1
+GROUP BY l.IDContenu
+ORDER BY 2 DESC;
 
 -- 25
 SELECT co.TitreContenu, a.Commentaire
